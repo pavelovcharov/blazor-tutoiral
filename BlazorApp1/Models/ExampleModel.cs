bad40dd3c596a1bc4d24cf0fb1036cc253965fee
ovcharov.pavel <1357165+pavelovcharov@users.noreply.github.com>
2019-11-18 13:07:34 +0300

    validation

---
+++ BlazorApp1/Models/ExampleModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace BlazorApp1.Models {
+    public class ExampleModel {
+        [Required]
+        [StringLength(10, ErrorMessage ="Name is tool long.")]
+        public string Name { get; set; }
+    }
+}
---
+++ BlazorApp1/Models/Starship.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorApp1.Models {
+    public class Starship {
+        [Required]
+        [StringLength(16, ErrorMessage ="Idenfier too long (16 charcter limit).")]
+        public string Identifier { get; set; }
+
+        public string Description { get; set; }
+
+        [Required]
+        public string Classification { get; set; }
+
+        [Range(1, 1000, ErrorMessage = "Accommodation invalid (1 - 1000).")]
+        public int MaximumAccommodation { get; set; }
+
+        [Required]
+        [Range(typeof(bool), "true", "true", ErrorMessage ="This form disallows unapproved ships.")]
+        public bool IsValidedDesign { get; set; }
+
+        [Required]
+        public DateTime ProductionDate { get; set; }
+    }
+}
